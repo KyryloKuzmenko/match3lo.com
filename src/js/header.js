53e34df5dcbaa98cd38f1d4b71afb8473caad7b7
Jango <kkuzmenko1992@gmail.com>
2025-07-14 17:26:38 -0230

    finished process section

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.querySelector('.burger-menu');
+  const closeMenu = document.querySelector('.close-menu');
   const backdrop = document.querySelector('.backdrop');
-  const headerContent = document.querySelector('.header-section .header-content');
   const links = document.querySelectorAll('.backdrop-list-item');
+  const logo = document.querySelector('.header-logo');
 
   menuBtn?.addEventListener('click', () => {
-    backdrop.classList.toggle('show');
-    headerContent.classList.toggle(backdrop.classList.contains('show'));
+    backdrop.classList.add('show');
+    menuBtn.classList.add('hidden');
+    closeMenu.classList.remove('hidden');
+    logo.classList.add('hidden');
+  });
+
+  closeMenu?.addEventListener('click', () => {
+    backdrop.classList.remove('show');
+    menuBtn.classList.remove('hidden');
+    closeMenu.classList.add('hidden');
+    logo.classList.remove('hidden');
   });
 
   backdrop?.addEventListener('click', e => {
     if (e.target === backdrop) {
       backdrop.classList.remove('show');
+      menuBtn.classList.remove('hidden');
+      closeMenu.classList.add('hidden');
+      logo.classList.remove('hidden');
     }
   });
 
   links.forEach(link => {
     link.addEventListener('click', () => {
       backdrop.classList.remove('show');
+      menuBtn.classList.remove('hidden');
+      closeMenu.classList.add('hidden');
+      logo.classList.remove('hidden');
     });
   });
 });
